3bf7203ee919a43f20dee86a6107698883127ca4
peteykienzzz <pmatczak2@gmail.com>
2022-04-14 04:47:38 -0700

    Inserting at the End

--- implementing_your_own_linked_list.py
+++ implementing_your_own_linked_list.py
@@ -7,10 +7,16 @@ class Node:
         return self.data
 
 class LinkedList:
-    def __init__(self):
+    def __init__(self, nodes=None):  # Here __init__() that allows you to quickly create linked lists with some data:
         self.head = None
+        if nodes is not None:
+            node = Node(data=nodes.pop(0))
+            self.head = node
+            for elem in nodes:
+                node.next = Node(data=elem)
+                node = node.next
 
-    def __repr__(self):
+    def __repr__(self):  # The __repr__ method will return the string that will print the linked list
         node = self.head
         nodes = []
         while node is not None:
@@ -31,3 +37,15 @@ class LinkedList:
         node.next = self.head
         self.head = node
 
+    def add_last(self, node):  # Inserting at the End
+        if self.head is None:
+            self.head = node
+            return
+        for current_node in self:
+            pass
+        current_node.next = node
+
+llist = LinkedList(['a', 'b','c'])
+
+llist.add_last(Node('d'))
+print(llist)
